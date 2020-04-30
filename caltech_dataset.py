0cba0fa8e9bf1431062dfb8b8fe55c1460be9677
Riccardo Mereu <riccardomereu5@gmail.com>
2020-04-30 22:43:53 +0200

    fixed again caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -25,8 +25,8 @@ class Caltech(VisionDataset):
         with open(os.path.join(self.root, f"{split}.txt")) as f:
             file_list = f.readlines()
         #remove class "BACKGROUND_Google"
-        file_list = [f for f in file_list if not f.startswith("BACKGROUND_Google")]
-
+        file_list = [f.strip('\n') for f in file_list if not f.startswith("BACKGROUND_Google/")]
+        #print(len(file_list))
         self.categories = set([f.split('/')[0] for f in file_list])
         self.categories = sorted(list(self.categories))
 
@@ -34,11 +34,13 @@ class Caltech(VisionDataset):
         self.index = []
 
         for (i, c) in enumerate(self.categories):
-            files = [f.split('/')[1].split('_')[1].strip('\n') 
-					 for f in file_list if f.startswith(c)]
+            files = [f.split('/')[1].split('_')[1]
+					 for f in file_list if f.startswith(f"{c}/")]
             n = len(files)
             self.index.extend(files)
             self.y.extend(n*[i])
+		
+        #print(len(self.index))
         #self.split = file_list
         # This defines the split you are going to use
         # (split files are called 'train.txt' and 'test.txt')
@@ -65,8 +67,10 @@ class Caltech(VisionDataset):
         img_name = os.path.join(self.root, "101_ObjectCategories",
                                 self.categories[self.y[idx]],
                                 f"image_{self.index[idx]}")
-        image = Image.open(img_name)
+        image = pil_loader(img_name)
         label = self.y[idx]
+		
+		
 
         #image, label =
 
@@ -76,8 +80,8 @@ class Caltech(VisionDataset):
 
         # Applies preprocessing when accessing the image
         if self.transform is not None:
-            image = self.transform(image)
-
+                image = self.transform(image)
+        
         return image, label
 
     def __len__(self):
